8a161c3f5ac4026ca62ecadbf47ac6f97e3b58cb
DabinLim <dabinim09@gmail.com>
2022-08-18 13:13:37 +0900

    다항 회귀

---
+++ README.md
@@ -0,0 +1,5 @@
+python                       3.7.13 <br/>
+matplotlib                   3.5.1 <br/>
+numpy                        1.21.6 <br/>
+scikit-learn                 1.0.2 <br/>
+tensorflow                   2.8.0 <br/>
--- linear_regression.py
+++ linear_regression.py
@@ -27,9 +27,38 @@ test_input = test_input.reshape(-1, 1)
 # 선형회귀 모델
 lr = LinearRegression()
 
-lr.fit(train_input, train_target)
+# lr.fit(train_input, train_target)
 
-print(lr.predict([[100]]))
+# print(lr.predict([[100]]))
 
 # 기울기를 통해 예측 - 계수(coefficient) 또는 가중치(weight)으로 표현
+# print(lr.coef_, lr.intercept_)
+
+plt.scatter(train_input, train_target)
+# # 15 ~ 50 까지 1차 방정식 그래프
+# plt.plot([15, 50], [15 * lr.coef_ + lr.intercept_,
+#          50 * lr.coef_ + lr.intercept_])
+
+# 2차 방정식 그래프를 위한 제곱 데이터
+train_poly = np.column_stack((train_input ** 2, train_input))
+test_poly = np.column_stack((test_input ** 2, test_input))
+
+# target값은 그대로 사용하여 훈련한다.
+lr.fit(train_poly, train_target)
+
+print(lr.predict([[50 ** 2, 50]]))
+
+# [  1.01433211 -21.55792498] 116.05021078278259
+# 모델이 학습한 그래프 : 무게 = 1.01 * 길이^2 - 21.6 * 116.05
+# 다항 회귀 (polynomial regression)
 print(lr.coef_, lr.intercept_)
+
+point = np.arange(15, 50)
+plt.plot(point, 1.01 * point ** 2 - 21.6 * point + 116.05)
+
+plt.scatter(50, 1574, marker='^')  # type: ignore
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+# 모델 파라미터 : 선형 회귀가 찾은 가중치처럼 머신러닝 모델이 특성에서 학습한 파라미터
